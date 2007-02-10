e5c563a1c0cbd9ed145465c22bd49fcd021e11f7
raster <raster>
2007-02-10 17:23:05 +0000

    patches applied as per emails :)

--- src/bin/embryo_cc_sc1.c
+++ src/bin/embryo_cc_sc1.c
@@ -21,7 +21,7 @@
  *  must not be misrepresented as being the original software.
  *  3.  This notice may not be removed or altered from any source
  *  distribution.  
- *  Version: $Id: embryo_cc_sc1.c,v 1.32 2006/07/10 18:31:18 raster Exp $
+ *  Version: $Id: embryo_cc_sc1.c,v 1.33 2007/02/10 17:23:05 raster Exp $
  */
 
 #ifdef HAVE_CONFIG_H
@@ -36,6 +36,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#ifdef WIN32
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#endif /* WIN32 */
+
 #include "embryo_cc_osdefs.h"
 #include "embryo_cc_sc.h"
 #include "embryo_cc_prefix.h"
@@ -314,7 +320,16 @@ sc_compile(int argc, char *argv[])
    if (!tmpdir) tmpdir = "/tmp";
 
    snprintf(outfname, _MAX_PATH, "%s/embryo_cc.asm-tmp-XXXXXX", tmpdir);
+#ifndef WIN32
    fd_out = mkstemp(outfname);
+#else
+   if (mktemp (outfname))
+     do
+       fd_out = open (outfname, O_CREAT | O_EXCL, S_IREAD | S_IWRITE);
+     while (!(fd_out == -1 && errno == EEXIST) && mktemp (outfname));
+   else
+     fd_out = -1;
+#endif /* WIN32 */
    if (fd_out < 0)
      error(101, outfname);
 
